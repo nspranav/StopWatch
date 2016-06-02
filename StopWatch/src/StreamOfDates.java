c08f2ed4207a7f262139ca199c301111b39c258b
PranavNS <PranavNS@pranavs-air.attlocal.net>
2016-06-02 14:16:21 -0400

    Commit done after the watch stops for 5 seconds

---
+++ StopWatch/src/StreamOfDates.java
@@ -0,0 +1,27 @@
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamOfDates {
+   
+	 static LocalDateTime myStopWatchFunction()
+	  {
+		  try{
+  	       Thread.sleep(1000);
+         } catch (InterruptedException e){
+             e.printStackTrace();
+  	   }
+  	   return LocalDateTime.now(); // get the current time
+	  }
+  
+	 public static void main(String[] args){
+
+	 
+ 
+    	   
+     // Generating a stream 	   
+     Stream<LocalDateTime> timeStream = Stream.generate(StreamOfDates::myStopWatchFunction).limit(5); 	   
+    	   
+     timeStream.forEach(System.out::println);   	   
+   }
+}
